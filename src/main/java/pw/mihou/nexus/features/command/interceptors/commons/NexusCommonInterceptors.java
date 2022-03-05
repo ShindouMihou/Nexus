ca6b4883cc902fd8b9a1186cc44d2ba8ace72d75
Shindou <kokocrunchsh@gmail.com>
2022-03-05 14:49:29 +0800

    Fix common interceptors not found in repository

--- src/main/java/pw/mihou/nexus/features/command/interceptors/commons/NexusCommonInterceptors.java
+++ src/main/java/pw/mihou/nexus/features/command/interceptors/commons/NexusCommonInterceptors.java
@@ -1,9 +1,8 @@
 package pw.mihou.nexus.features.command.interceptors.commons;
 
+import pw.mihou.nexus.features.command.interceptors.facades.NexusCommandInterceptor;
 import pw.mihou.nexus.features.messages.facade.NexusMessage;
 import pw.mihou.nexus.features.ratelimiter.core.NexusRatelimiterCore;
-
-import static pw.mihou.nexus.features.command.interceptors.facades.NexusCommandInterceptor.addMiddleware;
 
 /**
  * This class exists purely to separate the static function of adding
@@ -19,19 +18,19 @@ public class NexusCommonInterceptors {
     public static final String NEXUS_RATELIMITER = "nexus.ratelimiter";
 
     static {
-        addMiddleware(NEXUS_AUTH_BOT_OWNER_MIDDLEWARE, event -> event.stopIf(
+        NexusCommandInterceptor.addMiddleware(NEXUS_AUTH_BOT_OWNER_MIDDLEWARE, event -> event.stopIf(
                 !event.getUser().isBotOwner(),
                 NexusMessage.fromEphemereal("**PERMISSION DENIED**\nYou need to be the bot owner to execute this command.")
         ));
 
-        addMiddleware(NEXUS_AUTH_SERVER_OWNER_MIDDLEWARE, event -> event.stopIf(
+        NexusCommandInterceptor.addMiddleware(NEXUS_AUTH_SERVER_OWNER_MIDDLEWARE, event -> event.stopIf(
                 event.getServer().isPresent() && event.getServer().get().isOwner(event.getUser()),
                 NexusMessage.fromEphemereal("**PERMISSION DENIED**\nYou need to be the server owner to execute this command.")
         ));
 
-        addMiddleware(NEXUS_GATE_SERVER, event -> event.stopIf(event.getServer().isEmpty()));
-        addMiddleware(NEXUS_GATE_DMS, event -> event.stopIf(event.getServer().isPresent()));
-        addMiddleware(NEXUS_RATELIMITER, new NexusRatelimiterCore());
+        NexusCommandInterceptor.addMiddleware(NEXUS_GATE_SERVER, event -> event.stopIf(event.getServer().isEmpty()));
+        NexusCommandInterceptor.addMiddleware(NEXUS_GATE_DMS, event -> event.stopIf(event.getServer().isPresent()));
+        NexusCommandInterceptor.addMiddleware(NEXUS_RATELIMITER, new NexusRatelimiterCore());
     }
 
 }
