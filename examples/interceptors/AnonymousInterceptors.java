ec5f70374596b11d8e020feae94dddbcf8b2f173
Shindou Mihou <kokocrunchsh@gmail.com>
2022-07-22 14:53:18 +0800

    Adds examples for inline named interceptors and anonymous interceptors.

---
+++ examples/interceptors/AnonymousInterceptors.java
@@ -0,0 +1,23 @@
+package pw.mihou.nexus;
+
+import pw.mihou.nexus.core.reflective.annotations.Share;
+import pw.mihou.nexus.features.command.facade.NexusCommandEvent;
+import pw.mihou.nexus.features.command.facade.NexusHandler;
+import pw.mihou.nexus.features.command.interceptors.facades.NexusCommandInterceptor;
+
+import java.util.List;
+
+public class AnonymousInterceptors implements NexusHandler {
+
+    private final String name = "ping";
+    private final String description = "An example of how a command can share custom fields to a middleware, etc.";
+
+    private final List<String> middlewares = List.of(
+            NexusCommandInterceptor.middleware((event) -> event.stopIf(event.getServer().isEmpty()))
+    );
+
+    @Override
+    public void onEvent(NexusCommandEvent event) {
+        event.respondNow().setContent("Pong!").respond();
+    }
+}
