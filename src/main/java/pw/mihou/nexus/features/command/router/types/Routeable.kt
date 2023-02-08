78cba253f66ab13c004ad1f0d2e805398b73a41b
Shindou Mihou <git@mihou.pw>
2023-02-08 19:55:59 +0800

    fix: wrong type for routeable

--- src/main/java/pw/mihou/nexus/features/command/router/types/Routeable.kt
+++ src/main/java/pw/mihou/nexus/features/command/router/types/Routeable.kt
@@ -2,12 +2,12 @@ package pw.mihou.nexus.features.command.router.types
 
 import org.javacord.api.interaction.SlashCommandInteractionOption
 import pw.mihou.nexus.features.command.facade.NexusCommandEvent
-import java.util.function.Function
+import java.util.function.Consumer
 
 interface Routeable {
     fun accept(event: NexusCommandEvent, option: SlashCommandInteractionOption)
-    fun subcommand(name: String, from: SlashCommandInteractionOption, then: Function<SlashCommandInteractionOption, Void>) {
+    fun subcommand(name: String, from: SlashCommandInteractionOption, then: Consumer<SlashCommandInteractionOption>) {
         val subcommand = from.getOptionByName(name).orElse(null) ?: return
-        then.apply(subcommand)
+        then.accept(subcommand)
     }
 }
