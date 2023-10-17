f647e60ec591d0f23faeaf9d9d7263edb013fe23
Shindou Mihou <git@mihou.pw>
2023-10-17 22:38:37 +0800

    feat: add support for `Messageable`

--- src/main/java/pw/mihou/nexus/features/messages/ReactExtensions.kt
+++ src/main/java/pw/mihou/nexus/features/messages/ReactExtensions.kt
@@ -1,6 +1,8 @@
 package pw.mihou.nexus.features.messages
 
+import org.javacord.api.DiscordApi
 import org.javacord.api.entity.message.Message
+import org.javacord.api.entity.message.Messageable
 import org.javacord.api.event.message.CertainMessageEvent
 import pw.mihou.nexus.features.react.React
 import java.util.concurrent.CompletableFuture
@@ -18,6 +20,24 @@ fun CertainMessageEvent.R(react: React.() -> Unit): CompletableFuture<Message> {
     react(r)
 
     return r.messageBuilder!!.replyTo(message).send(channel).thenApply {
+        r.resultingMessage = it
+        return@thenApply it
+    }
+}
+
+/**
+ * An experimental feature to use the new Nexus.R rendering mechanism to render Discord messages
+ * with a syntax similar to a template engine that sports states (writable) that can easily update message
+ * upon state changes.
+ *
+ * @param react the entire procedure over how rendering the response works.
+ */
+@JvmSynthetic
+fun Messageable.R(api: DiscordApi, react: React.() -> Unit): CompletableFuture<Message> {
+    val r = React(api, React.RenderMode.Message)
+    react(r)
+
+    return r.messageBuilder!!.send(this).thenApply {
         r.resultingMessage = it
         return@thenApply it
     }
