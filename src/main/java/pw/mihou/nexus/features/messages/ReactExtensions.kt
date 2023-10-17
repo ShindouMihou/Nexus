f997db2332a992f2b5e11dbf094d4dc204799660
Shindou Mihou <git@mihou.pw>
2023-10-17 22:19:51 +0800

    feat: use `CertainMessageEvent` over `MessageCreateEvent` to ensure support over all message events

--- src/main/java/pw/mihou/nexus/features/messages/ReactExtensions.kt
+++ src/main/java/pw/mihou/nexus/features/messages/ReactExtensions.kt
@@ -1,7 +1,7 @@
 package pw.mihou.nexus.features.messages
 
 import org.javacord.api.entity.message.Message
-import org.javacord.api.event.message.MessageCreateEvent
+import org.javacord.api.event.message.CertainMessageEvent
 import pw.mihou.nexus.features.react.React
 import java.util.concurrent.CompletableFuture
 
@@ -9,10 +9,11 @@ import java.util.concurrent.CompletableFuture
  * An experimental feature to use the new Nexus.R rendering mechanism to render Discord messages
  * with a syntax similar to a template engine that sports states (writable) that can easily update message
  * upon state changes.
+ *
  * @param react the entire procedure over how rendering the response works.
  */
 @JvmSynthetic
-fun MessageCreateEvent.R(react: React.() -> Unit): CompletableFuture<Message> {
+fun CertainMessageEvent.R(react: React.() -> Unit): CompletableFuture<Message> {
     val r = React(this.api, React.RenderMode.Message)
     react(r)
 
