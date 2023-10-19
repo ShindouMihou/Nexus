2dc0ac2f6b09c6f0150a122d1d6914e1475785d3
Shindou Mihou <git@mihou.pw>
2023-10-19 16:18:39 +0800

    feat: add more operators for List for `Writable`

--- src/main/java/pw/mihou/nexus/features/react/writable/List.kt
+++ src/main/java/pw/mihou/nexus/features/react/writable/List.kt
@@ -29,3 +29,13 @@ operator fun <List, T> React.Writable<List>.minusAssign(element: T) where List :
 operator fun <List, T> React.Writable<List>.contains(element: T): Boolean where List : Collection<T> {
     return this.get().contains(element)
 }
+
+@Suppress("UNCHECKED_CAST")
+operator fun <List, T> React.Writable<List>.get(index: Int): T? where List : Collection<T> {
+    return when(val collection = this.get()) {
+        is kotlin.collections.List<*> -> collection[index] as? T
+        is MutableList<*> -> collection[index] as? T
+        is Array<*> -> collection[index] as? T
+        else -> collection.withIndex().find { (i, _) -> i == index } as? T
+    }
+}
